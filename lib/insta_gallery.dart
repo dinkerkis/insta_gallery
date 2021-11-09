bde88415d6e8e4fcdaeebd7f0ef5d7fcab2361c9
Samreen kaur <>
2021-11-09 14:09:36 +0530

    Updated code

--- lib/insta_gallery.dart
+++ lib/insta_gallery.dart
@@ -4,5 +4,6 @@ export 'package:insta_gallery/src/add_screen.dart';
 export 'package:insta_gallery/src/images_screen.dart';
 export 'package:insta_gallery/src/utils.dart';
 export 'package:insta_gallery/src/videos_screen.dart';
+export 'package:insta_gallery/src/video_editor.dart';
 export 'package:insta_gallery/src/widgets/bottom_bar.dart';
 export 'package:insta_gallery/src/widgets/top_bar.dart';
